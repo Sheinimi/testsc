62aeefdd67c3755bfc591d90adb5a1f0040cbbb3
Sheinimi <46713966+Sheinimi@users.noreply.github.com>
2019-12-10 17:21:36 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,5 @@
 @echo off
 PowerShell Set-ExecutionPolicy ByPass -Force
 PowerShell ./Install_requirements_2008R2.ps1 -Verbose
-PowerShell ./Install_winrm.ps1 -Verbose
 PowerShell Set-ExecutionPolicy Restricted
 Pause
