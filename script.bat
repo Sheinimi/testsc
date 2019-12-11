778342724e41adde9feeb9458cdbb04f32f5a086
Sheinimi <46713966+Sheinimi@users.noreply.github.com>
2019-12-11 14:37:32 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -11,12 +11,11 @@ call :reboot part2
 goto :eof
 
 :part2
-PowerShell ./Install_winrm.ps1 -Verbose
+PowerShell %currentpath%/Install_winrm.ps1 -Verbose
 echo "WinRM installed."
 PowerShell Set-ExecutionPolicy Restricted
 Pause
 goto :eof
-
 
 :reboot
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce /t REG_SZ /d "\"%~dpf0\" %~1" /v  RestartMyScript /f
